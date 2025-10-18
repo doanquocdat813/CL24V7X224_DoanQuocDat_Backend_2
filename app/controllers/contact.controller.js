bd3b8d58a19beb5313e1241e148de04543d4a956
doanquocdat813 <datcl24v7x224@dttx.ctu.edu.vn>
2025-10-18 11:46:35 +0700

    Update contact.controller.js

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -67,7 +67,7 @@ exports.update = async (req, res, next) => {
     const contactService = new ContactService(MongoDB.client);
     const document = await contactService.update(req.params.id, req.body);
     if (!document) {
-      return next(new ApiError(404, "Contact was updated successfully"));
+      return res.send({ message: "Contact was updated successfully" });
     }
     return res.send({ message: "Contact was updated successfully" });
   } catch (error) {
